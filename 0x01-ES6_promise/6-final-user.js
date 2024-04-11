e07fad9aa25bf1af2728ea9e9b66c30b6777cd97
Olakunle64 <olakunleisiaq50@gmail.com>
2024-04-11 16:02:03 +0100

    correct task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,5 +5,14 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const signUpPromise = signUpUser(firstName, lastName);
   const uploadPhotoPromise = uploadPhoto(fileName);
 
-  return Promise.allSettled([signUpPromise, uploadPhotoPromise]).then((results) => results);
+  return Promise.allSettled([signUpPromise, uploadPhotoPromise]).then((results) => {
+    const arr = [];
+    for (const result of results) {
+      if (result.status === 'rejected') {
+        arr.push({ status: result.status, value: result.reason });
+      }
+      arr.push({ status: result.status, value: result.value });
+    }
+    return arr;
+  });
 }
