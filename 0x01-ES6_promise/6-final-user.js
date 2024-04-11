8bfecb5b09732817686e592436e76ebd2c62edc0
Olakunle64 <olakunleisiaq50@gmail.com>
2024-04-11 02:21:46 +0100

    add task 7, 8 and 9

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,17 +2,19 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promise1 = signUpUser(firstName, lastName);
-  const promise2 = uploadPhoto(fileName);
-  return Promise.allSettled([promise1, promise2]).then((values) => {
-    const result = [];
-    values.forEach((value) => {
-      if (value.status === 'fulfilled') {
-        result.push(value.value);
-      } else {
-        result.push(value.reason);
-      }
-    });
-    return result;
-  });
+  const signUpPromise = signUpUser(firstName, lastName);
+  const uploadPhotoPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([signUpPromise, uploadPhotoPromise]).then((results) => results.map((result) => {
+    if (result.status === 'fulfilled') {
+      return {
+        status: 'fulfilled',
+        value: result.value,
+      };
+    }
+    return {
+      status: 'rejected',
+      value: result.reason,
+    };
+  }));
 }
