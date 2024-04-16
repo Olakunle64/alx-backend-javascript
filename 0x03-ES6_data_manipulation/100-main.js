d6c62cc852587ffc471ed2e1d94334201eaf440d
Olakunle64 <olakunleisiaq50@gmail.com>
2024-04-16 17:24:02 +0100

    throw an error when the query is is >= 5

--- 0x03-ES6_data_manipulation/100-main.js
+++ 0x03-ES6_data_manipulation/100-main.js
@@ -3,13 +3,13 @@ import { queryAPI, weakMap } from "./100-weak.js";
 const endpoint = { protocol: 'http', name: 'getUsers' };
 weakMap.get(endpoint);
 
-queryAPI(endpoint);
+queryAPI(endpoint); // 1
 console.log(weakMap.get(endpoint));
 
-queryAPI(endpoint);
+queryAPI(endpoint); // 2
 console.log(weakMap.get(endpoint));
 
-queryAPI(endpoint);
-queryAPI(endpoint);
-queryAPI(endpoint);
+queryAPI(endpoint); // 3
+queryAPI(endpoint); // 4
+queryAPI(endpoint); // 5
 queryAPI(endpoint);
--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -4,10 +4,10 @@ function queryAPI(endpoint) {
   if (!weakMap.has(endpoint)) {
     weakMap.set(endpoint, 1);
   } else {
-    if (weakMap.get(endpoint) >= 5) {
-      throw new Error('Endpoint load is high');
-    }
     weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  }
+  if (weakMap.get(endpoint) >= 5) {
+    throw new Error('Endpoint load is high');
   }
 }
 export { weakMap, queryAPI };
