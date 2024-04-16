f7b8a0a3a855f88fec4ccbbe45d5f25c3e06e38b
Olakunle64 <olakunleisiaq50@gmail.com>
2024-04-16 23:07:13 +0100

    debug task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,7 @@
 export default function cleanSet(set, startString) {
-  if (!startString) return [].join('-');
+  if ((!startString) && typeof startString != String) {
+    return [].join('-');
+  } 
 
   const suffixArray = [...set].filter((element) => element.startsWith(startString))
     .map((element) => element.slice(startString.length));
--- 0x03-ES6_data_manipulation/8-main.js
+++ 0x03-ES6_data_manipulation/8-main.js
@@ -1,4 +1,4 @@
 import cleanSet from "./8-clean_set.js";
 
-console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), 'bona'));
+console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), 1));
 console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), ''));
