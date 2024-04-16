c7b054b5c2ab845e90b5a95a8bc6a2cc1c1dedaf
Olakunle64 <olakunleisiaq50@gmail.com>
2024-04-16 23:21:29 +0100

    correct task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,9 @@
 export default function cleanSet(set, startString) {
-  if ((!startString) && typeof startString != String) {
-    return [].join('-');
-  } 
+  if ((!startString) && typeof startString !== 'string') {
+    return '';
+  }
 
-  const suffixArray = [...set].filter((element) => element.startsWith(startString))
+  const suffixArray = [...set].filter((element) => element && element.startsWith(startString))
     .map((element) => element.slice(startString.length));
 
   return suffixArray.join('-');
