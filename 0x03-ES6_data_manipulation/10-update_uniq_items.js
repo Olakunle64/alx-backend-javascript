dd5af9ad535d3be72b6d51d62da3df8b0baabb7e
Olakunle64 <olakunleisiaq50@gmail.com>
2024-04-16 16:32:01 +0100

    fix bugs

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -4,7 +4,7 @@ export default function updateUniqueItems(groceriesMap) {
   }
   groceriesMap.forEach((value, key) => {
     if (value === 1) {
-      groceriesMap.set(key, '100');
+      groceriesMap.set(key, 100);
     }
   });
   return groceriesMap;
