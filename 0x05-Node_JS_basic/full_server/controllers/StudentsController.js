93db61b3b2bc4f3f88343c41f002e63b9858d3fb
Olakunle64 <olakunleisiaq50@gmail.com>
2024-06-05 00:55:31 +0100

    get the database name from the command line

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -4,8 +4,9 @@ const readDatabase = require('../utils');
 
 class StudentsController {
   static getAllStudents(request, response) {
+    const databaseFilename = process.argv[2] || 'database.csv';
     const body = ['This is the list of our students'];
-    readDatabase('database.csv').then((fields) => {
+    readDatabase(databaseFilename).then((fields) => {
       const sortedKeys = Object.keys(fields).sort(
         (a, b) => a.toLowerCase().localeCompare(b.toLowerCase()),
       );
@@ -22,11 +23,12 @@ class StudentsController {
 
   static getAllStudentsByMajor(request, response) {
     const { major } = request.params;
+    const databaseFilename = process.argv[2] || 'database.csv';
     if (!['CS', 'SWE'].includes(major)) {
       response.status(500).send('Major parameter must be CS or SWE');
       return;
     }
-    readDatabase('database.csv').then((fields) => {
+    readDatabase(databaseFilename).then((fields) => {
       if (!fields[major]) {
         response.status(500).send('Cannot load the database');
         return;
