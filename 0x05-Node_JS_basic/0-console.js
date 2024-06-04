85f9788d295dfcdfb0b05d1fd94419308647f0e3
Olakunle64 <olakunleisiaq50@gmail.com>
2024-06-04 13:45:38 +0100

    use console.log instead of process.stdout.write

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 // This module contains a function that display a message to the stdout
 module.exports = function displayMessage(arg) {
-  if (typeof arg === 'string') process.stdout.write(`${arg}\n`);
+  console.log(arg)
 };
