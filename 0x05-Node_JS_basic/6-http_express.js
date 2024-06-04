d1de7c40004124866600d589b47ee2a0a5db8bde
Olakunle64 <olakunleisiaq50@gmail.com>
2024-06-04 17:09:06 +0100

    create http server with express.js

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// create HTTP server with express
+
+const express = require('express');
+
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(port);
