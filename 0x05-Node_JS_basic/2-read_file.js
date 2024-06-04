353fa10c61d389fa5f054156eafc9bc131e0ee6a
Olakunle64 <olakunleisiaq50@gmail.com>
2024-06-04 15:15:16 +0100

    read a file and log an output

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,40 @@
+#!/usr/bin/node
+// comment
+const fs = require('fs');
+
+function countStudents(filePath) {
+  if (fs.existsSync(filePath)) {
+    const data = fs.readFileSync(filePath, 'utf-8');
+    let rows = data.split('\n');
+    rows = rows.filter((row) => row);
+    console.log(`Number of students: ${rows.length - 1}`);
+    const fields = {};
+    for (let i = 1; i < rows.length; i += 1) {
+      const rowAttr = rows[i].split(',');
+      const field = rowAttr[rowAttr.length - 1];
+      const firstName = rowAttr[0];
+      if (field in fields) {
+        fields[field].count += 1;
+        fields[field].firstNames.push(firstName);
+      } else {
+        fields[field] = { count: 1, firstNames: [firstName] };
+      }
+    }
+    for (const [key, value] of Object.entries(fields)) {
+      console.log(`Number of students in ${key}: ${value.count}. List: ${value.firstNames.join(', ')}`);
+    }
+  } else {
+    throw new Error('Cannot load the database');
+  }
+}
+module.exports = countStudents;
+
+// filepath = "databae.csv"
+// if (fs.existsSync(filepath)) {
+//     const data = fs.readFileSync(filepath, 'utf-8');
+
+//     console.log(data.split('\n'))
+//     console.log(typeof data)
+// } else {
+//     console.log("not exists")
+// }
